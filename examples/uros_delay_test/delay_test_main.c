d78f181c115a7a3df43d8455d9ea703b615473b0
Viplav04 <55150569+Viplav04@users.noreply.github.com>
2019-11-25 14:50:05 +0100

    Custom delay msg - subscription issue

--- examples/uros_delay_test/delay_test_main.c
+++ examples/uros_delay_test/delay_test_main.c
@@ -2,24 +2,39 @@
 #include <rcl/error_handling.h>
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
-#include <std_msgs/msg/header.h>
+#include <delay_msgs/msg/delay.h>
 #include <stdio.h>
 #include <time.h>
 
-struct timespec send_time;
+struct timespec send_time;struct timespec receive_time;
 
-void msg_cb(const std_msgs__msg__Header* msg)
+int result = 0;
+
+int i = 1;
+// get empty wait set
+    uint32_t timeout_ms = 1000;
+void msg_cb(const delay_msgs__msg__Delay* msg)
 {
-    struct timespec receive_time;
+    
     clock_gettime(CLOCK_MONOTONIC, &receive_time);
 
-    printf("Current time: %d, %d\n", receive_time.tv_sec, receive_time.tv_nsec);
-    printf("Message time: %d, %d\n", msg->stamp.sec, msg->stamp.nanosec);
+  //  printf("Current time: %d, %d\n", receive_time.tv_sec, receive_time.tv_nsec);
+   // printf("Message time: %d, %d\n", msg->stamp.sec, msg->stamp.nanosec);
+    
+    int msg_time = msg->stamp.nanosec;
+    int rec_time = receive_time.tv_nsec;
 
-    float msg_time = (float)(msg->stamp.sec) + RCL_NS_TO_S((float)msg->stamp.nanosec);
-    float  rec_time = (float)(receive_time.tv_sec) + RCL_NS_TO_S((float)receive_time.tv_nsec);
+    int64_t result2 = msg->data8;
+    int64_t result3 = msg->data16;
+	int64_t result4 = msg->data32;
+	int64_t result5 = msg->data40;
+    int64_t result6 = msg->data48;
+	int64_t result7 = msg->data56;
+	int64_t result8 = msg->data64;
+ // float msg_time = (float)(msg->stamp.sec) + RCL_NS_TO_S((float)msg->stamp.nanosec);
+   // float rec_time = (float)(receive_time.tv_sec) + RCL_NS_TO_S((float)receive_time.tv_nsec);
 
-    printf("Delay: %f\n", (rec_time - msg_time));
+    printf("Delay: %d\n", (rec_time - msg_time));
 }
 
 #define CHECK_RET(FUNC) ret = FUNC ; if(ret != RMW_RET_OK) { fprintf(stderr, "Error invoking FUNC %d\n", ret); return -1; }
@@ -37,13 +52,13 @@ int delay_test_main(int argc, char* argv[])
 #endif
 {
     rcl_ret_t ret = RMW_RET_OK;
-    int result = 0;
+    
     
     printf("Initializing %s...", argv[0]);
     CHECK_RET(rclc_init(argc, argv));
     printf("done\n");
     
-    const rclc_message_type_support_t type_support = RCLC_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header);
+    const rclc_message_type_support_t type_support = RCLC_GET_MSG_TYPE_SUPPORT(delay_msgs, msg, Delay);
     rclc_node_t* node = NULL;
 
     printf("Creating node...");
@@ -57,7 +72,7 @@ int delay_test_main(int argc, char* argv[])
 
     rcl_subscription_t subscriber = rcl_get_zero_initialized_subscription();
     rcl_subscription_options_t subscription_ops = rcl_subscription_get_default_options();
-    const rosidl_message_type_support_t * sub_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header);
+    const rosidl_message_type_support_t * sub_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(delay_msgs, msg, Delay);
     
     ret = rcl_subscription_init(
         &subscriber,
@@ -84,14 +99,25 @@ int delay_test_main(int argc, char* argv[])
 
     
     sleep(2);
-
+ for(i=1;i<51;i++) {
+    
     result = clock_gettime(CLOCK_MONOTONIC, &send_time);
-    std_msgs__msg__Header msg = { .stamp.sec = send_time.tv_sec, .stamp.nanosec = send_time.tv_nsec };
+    
+    delay_msgs__msg__Delay msg;
+    msg.stamp.sec = send_time.tv_sec;
+    msg.stamp.nanosec = send_time.tv_nsec;
 
-    rclc_publish(publisher, &msg);
+    	msg.data8 = 9000000;
+	msg.data16 = 48651648;
+	msg.data32 = 87876867;
+	msg.data40 = 90001000;
+	msg.data48 = 48615668;
+	msg.data56 = 88768697;
+	msg.data64 = 87869858;
 
-    // get empty wait set
-    uint32_t timeout_ms = 1000;
+	rclc_publish(publisher, &msg);
+        
+ 
 
     rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
     ret = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, rcl_get_default_allocator());
@@ -120,6 +146,7 @@ int delay_test_main(int argc, char* argv[])
     ret = rcl_wait(&wait_set, RCL_MS_TO_NS(timeout_ms));
     if (ret == RCL_RET_TIMEOUT) {
         result = -1;
+        printf("rcl-wait failed");
         goto exit;
     }					
 
@@ -132,7 +159,7 @@ int delay_test_main(int argc, char* argv[])
     // if RET_OK => one subscription is received because only ONE subscription has been added
     // just double check here.
     if ( wait_set.subscriptions[0] ){
-        std_msgs__msg__Header sub_msg;
+        delay_msgs__msg__Delay sub_msg;
         rmw_message_info_t        messageInfo;
         ret = rcl_take(&subscriber, &sub_msg, &messageInfo);
 
@@ -150,7 +177,8 @@ int delay_test_main(int argc, char* argv[])
         fprintf(stderr, "[spin_node_once] no subscription received.\n");
     }
     rcl_wait_set_fini(&wait_set);
-            
+ 
+  }        
 
     printf("Done, terminating\n");
     rclc_destroy_publisher(publisher);
